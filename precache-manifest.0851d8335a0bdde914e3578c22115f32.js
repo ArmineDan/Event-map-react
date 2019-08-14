4b33d7e3bd3ecad61a6ba2c3d236c48795f0e68b
ArmineDan <39946460+ArmineDan@users.noreply.github.com>
2019-08-14 12:53:20 +0400

    Add files via upload

---
+++ precache-manifest.0851d8335a0bdde914e3578c22115f32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab632c557b9e499ec046e126550b5140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58691800221665268bb1",
+    "url": "/static/css/main.aa06f9fa.chunk.css"
+  },
+  {
+    "revision": "7097d6e40a6decc746b8",
+    "url": "/static/js/2.14083f6a.chunk.js"
+  },
+  {
+    "revision": "58691800221665268bb1",
+    "url": "/static/js/main.1912c862.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9557d7f1fbca006caa4e47e22d83ce0d",
+    "url": "/static/media/bee.9557d7f1.png"
+  }
+]);
